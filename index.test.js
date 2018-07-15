79345f24204eb9b937d8cc2bab3340a3321b38bb
Seanlangbrown <seanlangbrown@gmail.com>
2018-07-15 16:17:52 -0700

    implement mvp micro api and tests - responds to / with status 200

---
+++ index.test.js
@@ -0,0 +1,37 @@
+const supertest = require('supertest');
+const fs = require('fs');
+const micro = require('micro');
+//const pgController = require('');
+const newsAPI = require('./index.js');
+
+
+describe ('API Endpoints', () => {
+
+  let superAPI;
+  
+  beforeEach( async () => {
+    
+    superAPI = await supertest.agent(micro(newsAPI).listen());
+
+  });
+
+  afterEach( async () => {
+  });
+
+
+  test('It should respond to the / path with status 200 list of 10 articles', async () => {
+    //pgController.latest = jest.spyOn(spotQueueController, 'assign');
+    //pgController.latest.mockImplementation(() => ({id: null}));
+
+    let rootRequest = await superAPI.get('/');
+
+    expect(rootRequest.statusCode).toBe(200); //error or not?
+
+    //expect(pgController.assign).toHaveBeenCalled();
+
+  });
+
+
+  
+
+});
